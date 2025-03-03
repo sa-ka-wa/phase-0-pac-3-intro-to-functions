91a399c07e74c2713d1f5eb90b3bdf5836cf4ebd
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-03-03 10:40:24 +0300

    sayhello

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+  console.log("Hello!");
+}
+sayHello();
+
+function sayHelloToGuadalupe() {
+  console.log("Hello, Guadalupe!");
+}
+sayHelloToGuadalupe();
+
+function sayHelloToLiz() {
+  console.log("Hello, Liz!");
+}
+sayHelloToLiz();
+
+function sayHelloToSamip() {
+  console.log("Hello, Samip!");
+}
+sayHelloToSamip();
+
+sayHelloToGuadalupe();
+sayHelloToLiz();
+sayHelloToSamip();
+
+function sayHello(name) {
+  console.log(`Hello, ${name}!`);
+}
+sayHello("Guadalupe");
+sayHello("Liz");
+sayHello("Samip");
